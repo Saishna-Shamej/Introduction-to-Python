7e69c1fd54af4687696d39d01f9d32424ad128a2
Saishna-Shamej <126894625+Saishna-Shamej@users.noreply.github.com>
2023-12-13 09:51:00 +0530

    Add files via upload

---
+++ 1d&t.py
@@ -0,0 +1,17 @@
+import datetime
+x = datetime.datetime.now()
+print(x)
+print()
+
+#date output
+
+import datetime
+x = datetime.datetime.now()
+print(x.year)
+print(x.strftime("%A"))
+print(x.strftime('%a'))
+print()
+
+import datetime
+x = datetime.datetime(2023, 8, 18)
+print(x)
